23812a49979545e45cd0fde5f2490a45d44cbde0
Ryan James <ryan@plutovr.com>
2020-06-29 12:46:07 -0700

    add spinner animation

--- src/examples/pluto_office/scene.js
+++ src/examples/pluto_office/scene.js
@@ -2,8 +2,9 @@
 // important: test in metachromium which enables transparent WebXR rendering on desktop!
 // https://github.com/webaverse/metachromium-bin
 
-import { AmbientLight, AnimationMixer, Clock, LoadingManager, Scene, Color, Mesh, Plane, DoubleSide, SphereBufferGeometry, PlaneBufferGeometry, MeshBasicMaterial, MeshNormalMaterial, MeshPhongMaterial, Object3D, HemisphereLight, DirectionalLight, SpotLight, ShaderMaterial, AdditiveBlending, BufferGeometry, TextureLoader, Float32BufferAttribute, Points, DynamicDrawUsage, Vector3 } from "three";
+import { AmbientLight, AnimationMixer, Clock, LoadingManager, LoopOnce, Scene, Color, Mesh, Plane, DoubleSide, SphereBufferGeometry, PlaneBufferGeometry, MeshBasicMaterial, MeshNormalMaterial, MeshPhongMaterial, Object3D, HemisphereLight, DirectionalLight, SpotLight, ShaderMaterial, AdditiveBlending, BufferGeometry, TextureLoader, Float32BufferAttribute, Points, DynamicDrawUsage, Vector3, MixOperation } from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+import XRInput from "../../engine/xrinput";
 
 const GLTFPlutoOffice = require("./assets/models/pluto.glb");
 
@@ -16,11 +17,12 @@ scene.init = () =>
     scene.add( light );
 
     const loader = new GLTFLoader();
+    let spinnerAnimation;
     loader.load(GLTFPlutoOffice, function (gltf)
     {
         const office = gltf.scene;
         const animations = gltf.animations;
-        
+
         office.traverse( child => {
             if ( child.material ) child.material.metalness = 0;
         } );
@@ -34,9 +36,18 @@ scene.init = () =>
             mixer.update( delta );
         }
 
-        console.log("playing animation")
-        var action = mixer.clipAction( animations[ 0 ] ); // access first animation clip
-        action.play();
+        spinnerAnimation = mixer.clipAction( animations[ 0 ] ); // access first animation clip
+        spinnerAnimation.setLoop( LoopOnce )
+        spinnerAnimation.clampWhenFinished = true
+        spinnerAnimation.enable = true
+        spinnerAnimation.loop = false;
     });
+
+    XRInput.onSelect = e => {
+        if (spinnerAnimation) {
+            console.log("Playing spinner animation");
+            spinnerAnimation.play();
+        }
+    };
 }
 scene.init();
