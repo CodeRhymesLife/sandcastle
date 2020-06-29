65b2202b9d0e4133530747e2b6e67d3a35daf998
Ryan James <ryan@plutovr.com>
2020-06-29 12:37:43 -0700

    Add animation to spinner

--- src/examples/pluto_office/scene.js
+++ src/examples/pluto_office/scene.js
@@ -2,7 +2,7 @@
 // important: test in metachromium which enables transparent WebXR rendering on desktop!
 // https://github.com/webaverse/metachromium-bin
 
-import { AmbientLight, LoadingManager, Scene, Color, Mesh, Plane, DoubleSide, SphereBufferGeometry, PlaneBufferGeometry, MeshBasicMaterial, MeshNormalMaterial, MeshPhongMaterial, Object3D, HemisphereLight, DirectionalLight, SpotLight, ShaderMaterial, AdditiveBlending, BufferGeometry, TextureLoader, Float32BufferAttribute, Points, DynamicDrawUsage, Vector3 } from "three";
+import { AmbientLight, AnimationMixer, Clock, LoadingManager, Scene, Color, Mesh, Plane, DoubleSide, SphereBufferGeometry, PlaneBufferGeometry, MeshBasicMaterial, MeshNormalMaterial, MeshPhongMaterial, Object3D, HemisphereLight, DirectionalLight, SpotLight, ShaderMaterial, AdditiveBlending, BufferGeometry, TextureLoader, Float32BufferAttribute, Points, DynamicDrawUsage, Vector3 } from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 
 const GLTFPlutoOffice = require("./assets/models/pluto.glb");
@@ -18,12 +18,25 @@ scene.init = () =>
     const loader = new GLTFLoader();
     loader.load(GLTFPlutoOffice, function (gltf)
     {
-        gltf.scene.traverse( child => {
-
+        const office = gltf.scene;
+        const animations = gltf.animations;
+        
+        office.traverse( child => {
             if ( child.material ) child.material.metalness = 0;
         } );
 
-        scene.add(gltf.scene)
+        scene.add(office);
+
+        const mixer = new AnimationMixer( office );
+        const clock = new Clock();
+        office.Update = () => {
+            var delta = clock.getDelta(); // clock is an instance of THREE.Clock
+            mixer.update( delta );
+        }
+
+        console.log("playing animation")
+        var action = mixer.clipAction( animations[ 0 ] ); // access first animation clip
+        action.play();
     });
 }
 scene.init();
